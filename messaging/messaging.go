b9a0d7a351c8131253e395c7af0f596af7ce594f
Bogdan Melnik <teh.ld86@gmail.com>
2017-11-02 13:31:25 +0300

    Fix messaging with new sockets

--- messaging/messaging.go
+++ messaging/messaging.go
@@ -1,8 +1,10 @@
 package messaging
 
 import (
+    "os"
     "net"
     "log"
+    "syscall"
     "encoding/json"
 )
 
@@ -14,9 +16,8 @@ type Message struct {
 }
 
 type Messaging struct {
-    serverConnection *net.UDPConn
-    serverAddr *net.UDPAddr
-    mapping map[[20]byte]string
+    serverConnection net.PacketConn
+    mapping map[[20]byte]net.Addr
     bootstrap []string
     id [20]byte
 
@@ -30,9 +31,12 @@ func (messaging *Messaging) Serve() {
     for {
         var buffer [1024]byte
         var message Message
-        n, remoteAddr, _ := messaging.serverConnection.ReadFromUDP(buffer[:])
+        n, remoteAddr, _ := messaging.serverConnection.ReadFrom(buffer[:])
         json.Unmarshal(buffer[:n], &message)
-        messaging.mapping[message.FromId] = remoteAddr.String()
+
+        log.Printf("Remember node with id %v by remoteAddr %s", message.FromId, remoteAddr)
+
+        messaging.mapping[message.FromId] = remoteAddr
         messaging.InputMessages <- message
     }
 }
@@ -42,29 +46,15 @@ func (messaging *Messaging) handleOutputMessages() {
         select {
             case outputMessage := <-messaging.OutputMessages:
                 log.Printf("Trying to send message %v", outputMessage)
-                remoteIP, ok := messaging.mapping[outputMessage.ToId]
+                remoteAddr, ok := messaging.mapping[outputMessage.ToId]
                 if !ok {
                     log.Printf("Cannot find remote addr for node with id %s, skipping message")
                     continue
                 }
-                log.Printf("Found remoteIP %s by id %v", remoteIP, outputMessage.ToId)
+                log.Printf("Found remoteAddr %s by id %v", remoteAddr, outputMessage.ToId)
 
-                remoteAddr, err := net.ResolveUDPAddr("udp", remoteIP)
-                if err != nil {
-                    log.Printf("Cannot resolve %s, %s", remoteIP, err)
-                    continue
-                }
-
-                remoteConn, err := net.DialUDP("udp", messaging.serverAddr, remoteAddr)
-                if err != nil {
-                    log.Printf("Cannot connect to %s, %s", remoteAddr, err)
-                    continue
-                }
-
-                defer remoteConn.Close()
                 data, _ := json.Marshal(outputMessage)
-                log.Printf("%s", remoteConn)
-                remoteConn.Write(data)
+                messaging.serverConnection.WriteTo(data, remoteAddr)
         }
     }
 }
@@ -77,35 +67,52 @@ func (messaging *Messaging) doBootstrap() {
             continue
         }
 
-        remoteConn, err := net.DialUDP("udp", messaging.serverAddr, remoteAddr)
-        if err != nil {
-            log.Printf("Cannot connect to %s, %s", remoteIP, err)
-            continue
-        }
-        defer remoteConn.Close()
         message := Message{FromId: messaging.id, Action: "ping"}
         data, _ := json.Marshal(message)
-        remoteConn.Write(data)
+        messaging.serverConnection.WriteTo(data, remoteAddr)
     }
 }
 
+func createPacketConn() net.PacketConn {
+    fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+
+    if err != nil {
+        log.Fatalf("Cannot create socket, %s", err)
+    }
+
+    if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
+        log.Fatalf("Cannot set SO_REUSEADDR on socket, %s", err)
+    }
+
+    udpAddr, err := net.ResolveUDPAddr("udp", ":0")
+    if err != nil && udpAddr.IP != nil {
+        log.Fatalf("Cannot resolve addr, %s", err)
+    }
+
+    if err := syscall.Bind(fd, &syscall.SockaddrInet4{Port: udpAddr.Port}); err != nil {
+        log.Fatalf("Cannot bind socket, %s", err)
+    }
+
+    file := os.NewFile(uintptr(fd), string(fd))
+    conn, err := net.FilePacketConn(file)
+    if err != nil {
+        log.Fatalf("Cannot create connection from socket, %s", err)
+    }
+
+    if err = file.Close(); err != nil {
+        log.Fatalf("Cannot close dup file, %s", err)
+    }
+
+    return conn
+}
 
 func NewMessaging(bootstrap []string, id [20]byte) *Messaging {
-    serverAddr, err := net.ResolveUDPAddr("udp", ":0")
-    if err != nil {
-        log.Fatalf("Error on binding socket, %s", err)
-    }
-
-    serverConnection, err := net.ListenUDP("udp", serverAddr)
-    if err != nil {
-        log.Fatalf("Error on listening socket, %s", err)
-    }
+    serverConnection := createPacketConn()
 
     log.Printf("Waiting messages on %s", serverConnection.LocalAddr())
 
     return &Messaging{serverConnection: serverConnection,
-                      serverAddr: serverConnection.LocalAddr().(*net.UDPAddr),
-                      mapping: make(map[[20]byte]string),
+                      mapping: make(map[[20]byte]net.Addr),
                       InputMessages: make(chan Message),
                       OutputMessages: make(chan Message),
                       bootstrap: bootstrap,
